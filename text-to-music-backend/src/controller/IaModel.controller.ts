a7c933759bb43f0cb505bd91ecee0e3ca8245cf8
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-05-19 18:10:47 -0500

    feat: working backend to generate audio url

--- text-to-music-backend/src/controller/IaModel.controller.ts
+++ text-to-music-backend/src/controller/IaModel.controller.ts
@@ -1,20 +1,20 @@
-import { Request, Response } from "express";
-import  IaModelService  from "../services/IaModel.service";
-class IaModelController{
-    public async queryIaModel(req: Request, res: Response): Promise<Response>{
-        try{
-            const { query } = req.body;
-            console.log(query);
-            if(!query){
-                return res.status(400).json({ message: 'Query is required' });
-            }
-            const audioUrl = await IaModelService.queryIaModel(query);
-            return res.status(200).json({ audioUrl });
-        }catch(e){
-            console.log(e);
-            return res.status(500).json({ message: e });
-        }
+import { Request, Response } from "express"
+import IaModelService from "../services/IaModel.service"
+class IaModelController {
+  public async queryIaModel(req: Request, res: Response): Promise<Response> {
+    try {
+      const { inputs } = req.body
+      //console.log(req)
+      if (!inputs) {
+        return res.status(400).json({ message: "Query is required" })
+      }
+      const audioUrl = await IaModelService.queryIaModel(inputs)
+      return res.status(200).json({ audioUrl })
+    } catch (e) {
+      console.log(e)
+      return res.status(500).json({ message: e })
     }
+  }
 }
 
-export default new IaModelController();
+export default new IaModelController()
