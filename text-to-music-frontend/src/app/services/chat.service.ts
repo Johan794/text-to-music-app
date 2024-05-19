3f2e0fc81b7ecc005058a79900447a696b9eebbf
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-05-19 18:50:50 -0500

    add: angular frontend

---
+++ text-to-music-frontend/src/app/services/chat.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  private readonly url: string = 'http://localhost:3000/api/v1/generate-audio';
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(userMessage: string): Observable<{ audioUrl: string }>{
+    return this.http.post<{ audioUrl: string }>(this.url, { input: userMessage });
+  }
+}
