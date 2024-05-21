6cb336df4a9db7fdc39c49aff395fb97ce46a904
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-05-21 12:06:47 -0500

    change: dockerfile modified

--- text-to-music-backend/dist/index.js
+++ text-to-music-backend/dist/index.js
@@ -6,11 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: "*",
+}));
 (0, routes_1.default)(app);
 app.listen(port, () => {
     console.log(`Server is running on port ${port} in context path /api/v1`);
